5a5ce5da5769cdf7e078687724626038f74652e6
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-01-02 19:31:23 +0100

    ✨ completed: Flutter QR_Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:barcode_widget/barcode_widget.dart';
-// import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,10 +30,9 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
-
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _controller = TextEditingController();
+  final TextEditingController _controller = TextEditingController();
+  String qrResponse = " ";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,49 +41,52 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-                     Container(
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: const Colors.grey
-               
-            ),
-            child: TextFormField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                hintText: "Enter Text to generate QR Code",
-                focusedBorder: InputBorder.none,
-              ),
-            ),
-          ),
-            TextField(
-              onChanged: ((value) {
-                //print(":::::::::::::::::::onchanged value $value");
-              }),
-              decoration: InputDecoration(
-                hintText: "Search names",
-                suffixIcon: GestureDetector(
-                  onTap: () {},
-                  child: const Icon(
-                    Icons.cancel ,
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.grey.shade400,
+                ),
+                child: TextFormField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    hintText: "Enter Text to generate QR Code",
+                    focusedBorder: InputBorder.none,
                   ),
                 ),
               ),
             ),
-            const Text(
-              'You have pushed the button this many times:',
+            const SizedBox(height: 50),
+            BarcodeWidget(
+              barcode: Barcode.qrCode(),
+              data: _controller.text.trim(),
+              width: 200,
+              height: 200,
             ),
+            const SizedBox(height: 30),
+            Text(qrResponse),
+
+        
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async {
+          String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+              "#ff6666", "Cancel", true, ScanMode.QR);
+          print(barcodeScanRes);
+
+          setState(() {
+            qrResponse = barcodeScanRes;
+          });
+        },
+        tooltip: 'Scan QRCode',
+        label: const Text("Scan QRCode"),
+      ),
     );
   }
 }
