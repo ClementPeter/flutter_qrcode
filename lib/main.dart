601fd1ccaa463d2a390cf34facd36514a83c03c9
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2023-01-02 03:29:06 +0100

    💉 added : barcode & QR code dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'package:barcode_widget/barcode_widget.dart';
+// import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +30,10 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +44,22 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+                     Container(
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: const Colors.grey
+               
+            ),
+            child: TextFormField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: "Enter Text to generate QR Code",
+                focusedBorder: InputBorder.none,
+              ),
+            ),
+          ),
             TextField(
               onChanged: ((value) {
                 //print(":::::::::::::::::::onchanged value $value");
